b2c140de70d7e50907dfe4f2391e274c0c796240
SamuelTauleigne <stauleigne1@gmail.com>
2021-10-20 12:55:37 +0200

    Initialize Maven Project with Spring Boot

---
+++ src/main/java/fr/telecom_st_etienne/samuel_tauleigne/kanban_board_spring/KanbanBoardSpringApplication.java
@@ -0,0 +1,13 @@
+package fr.telecom_st_etienne.samuel_tauleigne.kanban_board_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KanbanBoardSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KanbanBoardSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/telecom_st_etienne/samuel_tauleigne/kanban_board_spring/KanbanBoardSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.telecom_st_etienne.samuel_tauleigne.kanban_board_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KanbanBoardSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
